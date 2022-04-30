f318eb9facd041a829432c707af163cb38fbca03
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-30 17:25:46 +0600

    login page  Social btn added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      {/* <Signin /> */}
       <Login />
       <Footer />
     </>
--- src/Pages/Signin/Signin.jsx
+++ src/Pages/Signin/Signin.jsx
@@ -2,9 +2,81 @@ import React from "react";
 
 const Signin = () => {
   return (
-    <div>
-      <h2>Sign in</h2>
-    </div>
+    <>
+      <div className=" flex justify-center items-center lg:h-[75vh] h-[100%]">
+        <div className="lg:w-[30%]   p-12 py-20 mb-20 shadow-lg shadow-blue-500/50">
+          <div>
+            <h2 className="text-center text-3xl font-bold pb-5">
+              Create an Account
+            </h2>
+            <p>To Keep connected with us. Please To Create an Account</p>
+          </div>
+          <form>
+            <div class="grid gap-6 mb-6 lg:grid-cols-2"></div>
+            <div class="mb-6">
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter your Email"
+                required
+              />
+            </div>
+            <div class="mb-6">
+              <label
+                for="password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter Password"
+                required
+              />
+            </div>
+            <div class="mb-6">
+              <label
+                for="password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Confirm Password"
+                required
+              />
+            </div>
+            {/* create and account button */}
+            <div className="mb-3">
+              <p class=" inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg  bg-gradient-to-br from-green-400 to-blue-600 ">
+                <span class="relative px-2 cursor-pointer  bg-white rounded-md ">
+                  Already have account
+                </span>
+              </p>
+            </div>
+
+            <button
+              type="submit"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-9 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Create Account
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
