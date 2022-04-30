96875fc9881d141645e92037a2c9e4953b31dfdc
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-30 17:05:04 +0600

    login Page Design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
 import "../node_modules/flowbite/dist/flowbite.js";
 import Footer from "./Shared/Footer/Footer";
+import Navbar from "./Shared/Navbar/Navbar";
+import Login from "./Pages/Login/Login";
+import Signin from "./Pages/Signin/Signin";
 
 function App() {
-  return <>{/* <Footer /> */}</>;
+  return (
+    <>
+      <Navbar />
+      <Login />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
