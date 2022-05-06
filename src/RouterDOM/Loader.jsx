f1547f25bba7574da7bd4430231ae31cef2db73e
morshedulmunna <morshedulmunna1@gmail.com>
2022-05-06 23:31:11 +0600

    view Item fix

--- src/RouterDOM/Loader.jsx
+++ src/RouterDOM/Loader.jsx
@@ -3,7 +3,7 @@ import loader from "../Images/loading.svg";
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-60 w-full h-[100vh]">
+    <div className="flex justify-center items-center  w-full h-[100vh]">
       <img src={loader} alt="" />
     </div>
   );
